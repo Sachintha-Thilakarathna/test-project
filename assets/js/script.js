547ad456d146e5251fb8e96470e8acbffad63be6
Sachintha-Thilakarathna <thilakarathnasachintha@gmail.com>
2025-05-18 12:47:50 +0530

    second day

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,22 @@ const light = document.getElementById("light");
 
 dark.addEventListener("click",function(){
     const body = document.querySelector("body")
+    const testomnial = document.getElementById("testomonial")
     body.classList.add("darkMode")
+    body.classList.remove("text-dark")
     body.classList.add("text-primary")
     body.classList.remove("bg-white")
+    testomonial.classList.add("darkMode")
+    testomonial.classList.remove("bg-white")
 })
 
 light.addEventListener("click",function(){
     const body = document.querySelector("body")
+    const testomonial = document.getElementById("testomonial")
     body.classList.add("bg-white")
-    body.classList.add("text-primary")
+    body.classList.add("text-dark")
     body.classList.remove("darkMode")
+    testomonial.classList.add("bg-white")
+    testomonial.classList.remove("darkMode")
 })
 
